54c1da3a2f150114b11bef7b3bccece3700b5ede
Senthura pandi <pandimads@gmail.com>
2023-07-10 00:33:25 +0530

    this is my first push to git

--- mongodbtest.py
+++ mongodbtest.py
@@ -8,6 +8,32 @@ d = {
     "email":"rvani1262@gmail.com",
     "age":"22"
 }
+
+d = {
+    "name":"senthura",
+    "email":"rvani1262@gmail.com",
+    "age":"22"
+}
+d = {
+    "name":"senthura",
+    "email":"rvani1262@gmail.com",
+    "age":"22"
+}
+d = {
+    "name":"senthura",
+    "email":"rvani1262@gmail.com",
+    "age":"22"
+}
+d = {
+    "name":"senthura",
+    "email":"rvani1262@gmail.com",
+    "age":"22"
+}
+d = {
+    "name":"senthura",
+    "email":"rvani1262@gmail.com",
+    "age":"22"
+}
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d)
